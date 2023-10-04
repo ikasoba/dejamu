aa87e7bd97d43d4493d25e66ac280c77aed3be5e
ikasoba <57828948+ikasoba@users.noreply.github.com>
2023-10-04 15:32:04 +0900

    新しく

--- dejamu.config.ts
+++ dejamu.config.ts
@@ -3,11 +3,13 @@ import "dejamu/plugins/global.d.ts";
 import type { Config } from "dejamu/mod.ts";
 import PreactPlugin from "dejamu/plugins/preact/mod.ts";
 import MarkdownPlugin from "dejamu/plugins/md/mod.ts";
+import PostCssPlugin from "dejamu/plugins/postcss/mod.ts";
 
 export default {
-  entryPoints: ["pages/*.{jsx,tsx,md}"],
+  entryPoints: ["pages/*.{jsx,tsx,md}", "styles/*.css"],
   plugins: [
     PreactPlugin(),
     MarkdownPlugin("layouts/"),
+    PostCssPlugin([".css"]),
   ],
 } satisfies Config;
--- layouts/DejamuTopPage.tsx
+++ layouts/DejamuTopPage.tsx
@@ -4,19 +4,11 @@ import { LayoutComponent } from "dejamu/plugins/md/MarkdownPlugin.tsx";
 import Counter from "../components/Counter.tsx";
 
 export default (function DejamuTopPage({ children }) {
-  if (isBrowser == false) {
-    // cssには対応してないので力技で
-    try {
-      Deno.mkdirSync(".out/");
-    } catch {}
-    Deno.copyFileSync("styles/index.css", ".out/index.css");
-  }
-
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="stylesheet" href="index.css" />
+        <link rel="stylesheet" href="styles/index.css" />
         <title>Dejamu🛌</title>
       </Head>
       <Markdown>
