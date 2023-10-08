7579f690c37c45a76e23e2e058937d0df63410a0
ikasoba <57828948+ikasoba@users.noreply.github.com>
2023-10-09 00:55:45 +0900

    コードのハイライトに対応

--- dejamu.config.ts
+++ dejamu.config.ts
@@ -2,12 +2,20 @@ import type { Config } from "dejamu/mod.ts";
 import PreactPlugin from "dejamu/plugins/preact/mod.ts";
 import MarkdownPlugin from "dejamu/plugins/md/mod.ts";
 import TwindPlugin from "dejamu/plugins/twind/mod.ts";
+import HljsPlugin from "dejamu/plugins/md/hljs/mod.ts";
 
 export default {
   entryPoints: ["pages/**/*.{jsx,tsx,md}"],
   plugins: [
     PreactPlugin(),
-    MarkdownPlugin("layouts/"),
+    MarkdownPlugin({
+      layouts: "layouts/",
+      plugins: [
+        HljsPlugin({
+          theme: "github-dark-dimmed",
+        }),
+      ],
+    }),
     TwindPlugin({
       styles: ["styles/*.css"],
     }),
