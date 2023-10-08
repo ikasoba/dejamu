d0b154b21ca27039360b4e3ff39d7faac0a214c8
ikasoba <57828948+ikasoba@users.noreply.github.com>
2023-10-08 14:37:26 +0900

    クライアントサイドでもclassNameでの指定を可能性に & styleを使い回すように

--- plugins/twind/TwindPlugin.tsx
+++ plugins/twind/TwindPlugin.tsx
@@ -1,40 +1,65 @@
-import { extract, install, TwindConfig } from "@twind/core";
+import { css, extract, setup, tw, TwindConfig } from "@twind/core";
 import * as path from "../../deps/path.ts";
+import * as fs from "../../deps/fs.ts";
 import { DejamuPlugin } from "../../pluginSystem/Plugin.ts";
 import { appendHead } from "../Head.tsx";
+import { glob } from "../../utils/glob.ts";
+
+export interface TwindPluginConfig {
+  /** glob patterns for css files. */
+  styles?: string[];
+
+  /** glob pattern for a config file. */
+  config?: string;
+}
 
 /**
  * @param configPath -
  */
-export function TwindPlugin(configPath: string): DejamuPlugin {
+export function TwindPlugin(
+  {
+    styles = [],
+    config: configPath = "./twind.config.{ts,js,tsx,jsx}",
+  }: TwindPluginConfig,
+): DejamuPlugin {
   let config: TwindConfig;
 
   return {
     type: "dejamu",
     plugin: {
       async onReady() {
-        configPath = await import.meta.resolve(
-          path.toFileUrl(path.resolve(path.join(Deno.cwd(), configPath)))
-            .toString(),
+        configPath = await glob(configPath).then(
+          (x) => path.toFileUrl(x[0].path).toString(),
         );
-        config = (await import(configPath)).default;
 
-        install(config);
+        config = (await import(
+          configPath
+        )).default;
+
+        setup(config);
+
+        const cssFiles = (await Promise.all(styles.map((x) => glob(x)))).flat();
+        for (const file of cssFiles) {
+          tw(
+            css(await Deno.readTextFile(file.path)),
+          );
+        }
       },
       onRender(body, script) {
         const { html, css } = extract(body);
 
         appendHead(
-          <style dangerouslySetInnerHTML={{ __html: css }}>
+          <style id="__DJM_TWIND__" dangerouslySetInnerHTML={{ __html: css }}>
           </style>,
         );
 
         script.head.unshift(
-          'import { install as twind_install } from "@twind/core"',
+          'import { hydrate as twind_hydrate } from "dejamu/plugins/twind/hydrate.ts"',
           `import twindPlugin_twindConfig from ${JSON.stringify(configPath)}`,
         );
-        script.footer.unshift(
-          "twind_install(twindPlugin_twindConfig);",
+
+        script.body.unshift(
+          "twind_hydrate(twindPlugin_twindConfig);",
         );
 
         return html;
